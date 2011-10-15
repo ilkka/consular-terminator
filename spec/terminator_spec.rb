3d0f7f6174f8b48569bc0fa7364eb9b5d9adf059
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 17:03:37 +0300

    Check that linux is a valid platform
    Depends on the method checking via RbConfig

--- spec/terminator_spec.rb
+++ spec/terminator_spec.rb
@@ -1,7 +1,13 @@
 require File.expand_path('../spec_helper', __FILE__)
+require 'rbconfig'
 
 describe Consular::Terminator do
   it 'should be added as a core to Consular' do
     Consular.cores.should include Consular::Terminator
+  end
+
+  it `should treat linux as a valid system` do
+    RbConfig::CONFIG.any_instance.expects(:[]).with('host_os').returns('x86-linux')
+    Consular::Terminator.valid_system?.should be true
   end
 end
