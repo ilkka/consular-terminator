a4d5d84bbe5f0bd4e0832792aa1f3cebbebbf977
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 08:17:01 -0700

    Fix RbConfig::CONFIG mocking

--- spec/terminator_spec.rb
+++ spec/terminator_spec.rb
@@ -6,8 +6,13 @@ describe Consular::Terminator do
     Consular.cores.should include Consular::Terminator
   end
 
-  it `should treat linux as a valid system` do
-    RbConfig::CONFIG.any_instance.expects(:[]).with('host_os').returns('x86-linux')
-    Consular::Terminator.valid_system?.should be true
+  it 'should treat linux as a valid system' do
+    RbConfig::CONFIG.expects(:[]).returns({ 'host_os' => 'x86-linux' })
+    Consular::Terminator.valid_system?.should == true
+  end
+
+  it 'should treat non-linux as an invalid system' do
+    RbConfig::CONFIG.expects(:[]).with('host_os').returns('darwin')
+    Consular::Terminator.valid_system?.should == false
   end
 end
