d69d1b3b79dd2f9668ec684496356eaa5a00d61e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 16:30:57 +0300

    Fix spec, it's 'should include', not 'should_include'

--- spec/terminator_spec.rb
+++ spec/terminator_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path('../spec_helper', __FILE__)
 
 describe Consular::Terminator do
   it 'should be added as a core to Consular' do
-    Consular.cores.should_include Consular::Terminator
+    Consular.cores.should include Consular::Terminator
   end
 end
