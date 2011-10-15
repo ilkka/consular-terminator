19dff88fde863e6b58efcb1f473dfc1b184ff144
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 17:02:35 +0300

    Require mocha for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ Spork.prefork do
   $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
   $LOAD_PATH.unshift(File.dirname(__FILE__))
   require 'rspec'
+  require 'mocha'
 
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
