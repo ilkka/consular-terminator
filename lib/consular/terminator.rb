997b282334a0418e9e62c2068484c91d2e86cfd9
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 08:44:33 -0700

    Implement valid_system?

--- lib/consular/terminator.rb
+++ lib/consular/terminator.rb
@@ -1,4 +1,5 @@
 require 'consular'
+require 'rbconfig'
 
 module Consular
   # Consular core for interacting with Terminator
@@ -7,5 +8,14 @@ module Consular
 
     Consular.add_core self
 
+    class << self
+      # Check to see if current system is valid for this core
+      #
+      # @api public
+      def valid_system?
+        ((RbConfig::CONFIG['host_os'] =~ /linux/) != nil) &&
+          !`which xdotool`.chomp.empty?
+      end
+    end
   end
 end
