4912742ebc22ca5997ab827d5c2db70f2bb90dcb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 16:19:26 +0300

    Cucumber env file

---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+require 'spork'
+
+Spork.prefork do
+  require 'bundler'
+  begin
+    Bundler.setup(:default, :development)
+  rescue Bundler::BundlerError => e
+    $stderr.puts e.message
+    $stderr.puts "Run `bundle install` to install missing gems"
+    exit e.status_code
+  end
+
+  require 'rspec/expectations'
+end
+
+Spork.each_run do
+
+end
