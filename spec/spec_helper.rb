769cac2bf2612f06340d0fd492e958a785c5c26a
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-10-15 16:16:46 +0300

    Spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'spork'
+
+Spork.prefork do
+  $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+  require 'rspec'
+
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+  RSpec.configure do |config|
+  end
+end
+
+Spork.each_run do
+  require 'consular/terminator'
+end
+
